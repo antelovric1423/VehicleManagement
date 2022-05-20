0f2b45e445b1319f7c503998eeea18c0e04a573d
Ante Lovric <ante.lovric77@gmail.com>
2022-05-20 23:11:51 +0200

    Frontend server initial commit

---
+++ vehicle-management-frontend/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Vehicle management</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #c5c5c5;
+  margin-top: 60px;
+}
+
+body {
+  background-color: #3f3f3f;
+}
+</style>
