b7ce2de9789f62186fc52812ab707bd28333e98c
Ante Lovric <ante.lovric77@gmail.com>
2022-05-24 20:48:15 +0200

    Validate post request with JOI

--- backend/vehicles/controller.js
+++ backend/vehicles/controller.js
@@ -1,5 +1,7 @@
 import sequelize from "../sequelize";
 import { getIdParam } from "../helpers";
+import { vehiclePostSchema } from "./validation";
+import Joi from "joi"
 
 export async function vehiclesGetAll(req, res) {
     const vehicles = await sequelize.models.Vehicles.findAll({ attributes: ["id", "make", "model", "year"] });
@@ -7,17 +9,10 @@ export async function vehiclesGetAll(req, res) {
 }
 
 export async function vehiclesPost(req, res) {
-    const numAllowedParams = 3
-    if (!req.body.make || !req.body.model || !req.body.year) {
-        res.status(400).send("Request does not contain required parameters")
-        return
-    }
-    if (Number.isNaN(Number(req.body.year)) || typeof (req.body.make) != "string" || typeof (req.body.model) != "string") {
-        res.status(400).send("Request contains invalid parameter values")
-        return
-    }
-    if (Object.keys(req.body).length != numAllowedParams) {
-        res.status(400).send("Request contains non-required parameters")
+    try {
+        Joi.assert(req.body, vehiclePostSchema)
+    } catch (error) {
+        res.status(400).send("Invalid request")
         return
     }
 
@@ -28,12 +23,9 @@ export async function vehiclesPost(req, res) {
     }
 
     const vehicles = await sequelize.models.Vehicles.findAll({
-        where: {
-            make: req.body.make,
-            model: req.body.model,
-            year: Number(req.body.year)
-        }
+        where: { ...vehicle }
     })
+
     if (vehicles.length != 0) {
         res.status(400).send("Item already exists")
         return
